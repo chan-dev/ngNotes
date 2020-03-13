f1507267550608e8034f97fbff9318bfbff14c5f
christian m. austria <christian.m.austria@gmail.com>
2020-03-13 09:39:13 +0800

    Setup environment
    Install and setup required packages and add changes to typescript config

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,10 @@
 export const environment = {
-  production: true
+  production: true,
+  apiKey: 'AIzaSyDiwlK8Ru5NWYikrarRu9zvyY656bMulQQ',
+  authDomain: 'ngnotes-a4ac3.firebaseapp.com',
+  databaseURL: 'https://ngnotes-a4ac3.firebaseio.com',
+  projectId: 'ngnotes-a4ac3',
+  storageBucket: 'ngnotes-a4ac3.appspot.com',
+  messagingSenderId: '1079766222530',
+  appId: '1:1079766222530:web:2a2bee7e6e2d9a14fb9d5b',
 };
