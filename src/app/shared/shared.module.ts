460a90f39923c94e3ea3fd3a2f06e07039ebda87
christian m. austria <christian.m.austria@gmail.com>
2020-03-19 09:00:03 +0800

    Add primeng chips module

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,8 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { TooltipModule } from 'ngx-bootstrap/tooltip';
 import { SimplebarAngularModule } from 'simplebar-angular';
+import { ChipsModule } from 'primeng/chips';
 
 @NgModule({
   declarations: [],
@@ -11,12 +13,16 @@ import { SimplebarAngularModule } from 'simplebar-angular';
     FontAwesomeModule,
     TooltipModule.forRoot(),
     SimplebarAngularModule,
+    ChipsModule,
+    FormsModule,
   ],
   exports: [
     CommonModule,
     FontAwesomeModule,
     TooltipModule,
     SimplebarAngularModule,
+    ChipsModule,
+    FormsModule,
   ],
 })
 export class SharedModule {}
