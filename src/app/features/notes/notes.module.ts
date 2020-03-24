c388afc1ab6a0a4f87bfa123f57fa93f7b847926
chan-dev <christian.m.austria@gmail.com>
2020-03-24 15:25:57 +0800

    Update localStorage sync for notes to include tags

--- src/app/features/notes/notes.module.ts
+++ src/app/features/notes/notes.module.ts
@@ -24,6 +24,7 @@ import { UserDisplayComponent } from './components/user-display/user-display.com
 
 const metaReducers = [
   createLocalStorageSyncReducer({
+    keys: [{ notes: ['items', 'sharedItems', 'tags'] }],
     keys: [{ notes: ['items', 'sharedItems'] }],
     rehydrate: true,
   }),
